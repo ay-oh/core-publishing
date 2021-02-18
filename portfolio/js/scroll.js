22e0e38df57788fae1195122c8e853dd28d77938
cororing <corock.dev@gmail.com>
2021-02-18 18:19:16 +0900

    제이쿼리 추가

--- portfolio/js/scroll.js
+++ portfolio/js/scroll.js
@@ -2,6 +2,17 @@
 
 // });
 
+// trigger
+$('.trigger').click(() => {
+    $(this).toggleClass('active');
+    $('.gnb').toggleClass('active');
+});
+
+// 클릭 시
+$('gnb a').click(() => {
+    $('.gnb, .trigger').removeClass('active');
+});
+
 $(window).scroll(function () {
     // 스크롤 상단이 50px보다 크다면
     if ($(window).scrollTop() > 50) {
